91b46be1ef1341271bb2d52620e7136ab1f26c61
Yasmin Pires <160610423+yaspires@users.noreply.github.com>
2025-09-17 13:12:38 -0300

    Feature/login cadastro (#261)
    * feat(auth): adiciona lógica de cadastro + login com JWT
    
    * feat(integração): integra cadastro e login com o front-end. Fecha #6
    
    * fix(integração): corrige erro no login (#6)
    
    * Corrige erro de duplicação de form e altera politica de segurança
    
    * fix:Corrige pacote defeituoso
    Login e cadastro funcionando
    
    * fix:Corrige pacote defeituoso
    corrige erro de mapa
    
    * Remove console logs from fetchFaq function
    
    Removed console logs for API response and URL.
    
    ---------
    
    Co-authored-by: Daniel Augusto Mandira <105872910+DanielMandira@users.noreply.github.com>

--- back-end/src/models/Usuario.js
+++ back-end/src/models/Usuario.js
@@ -1,35 +1,40 @@
 import { Sequelize } from "sequelize";
-import connection from '../config/sequelize-config.js'
+import connection from "../config/sequelize-config.js";
 
-const Usuario = connection.define("usuario", {
+const User = connection.define('usuario', {
     id: {
         type: Sequelize.INTEGER,
-        primaryKey: true,
         autoIncrement: true,
+        primaryKey: true
     },
     nome: {
-        type: Sequelize.TEXT,
-        allowNull: false,
+        type: Sequelize.STRING(100),
+        allowNull: false
     },
     email: {
-        type: Sequelize.TEXT,
+        type: Sequelize.STRING(100),
         allowNull: false,
+        unique: true
     },
     senha: {
-        type: Sequelize.TEXT,
-        allowNull: false,
+        type: Sequelize.STRING(255),
+        allowNull: false
     },
     nivel: {
-        type: Sequelize.TEXT,
+        type: Sequelize.TINYINT,
         allowNull: false,
+        defaultValue: 1
     },
     celular: {
-        type: Sequelize.TEXT,
-        allowNull: false,
-    }},
-    {
+        type: Sequelize.STRING(20),
+        allowNull: true
+    }
+}, {
     tableName: 'usuario',
-    timestamps: false, 
+    timestamps: false,
 });
 
-export default Usuario;
+User.sync({ force: false });
+
+export default User;
+
